38bb72422f7724a65f288368e96eaeebca28c4e0
Ibrahim9878 <talibliibrahim28@gmail.com>
2024-08-24 21:05:04 +0400

    Add project files.

---
+++ Sys. Prog. Task 1 Task Manager/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Sys._Prog._Task_1_Task_Manager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Sys. Prog. Task 1 Task Manager/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Sys._Prog._Task_1_Task_Manager;
+
+public partial class MainWindow : Window, INotifyPropertyChanged
+{
+    private ObservableCollection<Process> processList;
+
+    DispatcherTimer _timer;
+    private void StartAutoRefresh()
+    {
+        _timer = new DispatcherTimer();
+        _timer.Interval = TimeSpan.FromSeconds(5);
+        _timer.Tick += (sender, e)=> { ProcessList = new(Process.GetProcesses().ToList()); };
+        _timer.Start();
+    }
+
+    public ObservableCollection<Process> ProcessList { get => processList; set { processList = value; OnPropertyChanged(); }}
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+    public MainWindow()
+    {
+        ProcessList = new (Process.GetProcesses().ToList());
+        foreach (Process process in ProcessList)
+        {
+            process.
+        }
+        
+        InitializeComponent();
+        StartAutoRefresh();
+        DataContext = this;
+    }
+}
