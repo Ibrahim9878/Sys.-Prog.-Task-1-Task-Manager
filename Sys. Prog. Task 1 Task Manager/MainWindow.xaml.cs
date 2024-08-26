7aee8b83f00c5db17c3fc7ed3816613a57925437
Ibrahim9878 <talibliibrahim28@gmail.com>
2024-08-26 20:14:03 +0400

    Task manager 2

--- Sys. Prog. Task 1 Task Manager/MainWindow.xaml.cs
+++ Sys. Prog. Task 1 Task Manager/MainWindow.xaml.cs
@@ -10,17 +10,19 @@ namespace Sys._Prog._Task_1_Task_Manager;
 public partial class MainWindow : Window, INotifyPropertyChanged
 {
     private ObservableCollection<Process> processList;
+    private List<Process> BlackList;
+
 
     DispatcherTimer _timer;
     private void StartAutoRefresh()
     {
         _timer = new DispatcherTimer();
         _timer.Interval = TimeSpan.FromSeconds(5);
-        _timer.Tick += (sender, e)=> { ProcessList = new(Process.GetProcesses().ToList()); };
+        _timer.Tick += (sender, e) => { ProcessList = new(Process.GetProcesses().ToList()); };
         _timer.Start();
     }
 
-    public ObservableCollection<Process> ProcessList { get => processList; set { processList = value; OnPropertyChanged(); }}
+    public ObservableCollection<Process> ProcessList { get => processList; set { processList = value; OnPropertyChanged(); } }
 
     public event PropertyChangedEventHandler? PropertyChanged;
     protected void OnPropertyChanged([CallerMemberName] string name = null)
@@ -29,14 +31,35 @@ public partial class MainWindow : Window, INotifyPropertyChanged
     }
     public MainWindow()
     {
-        ProcessList = new (Process.GetProcesses().ToList());
+        ProcessList = new(Process.GetProcesses().ToList());
         foreach (Process process in ProcessList)
         {
-            process.
+
         }
-        
+
         InitializeComponent();
         StartAutoRefresh();
         DataContext = this;
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        RunNewTaskWindow runNewTaskWindow = new RunNewTaskWindow();
+        runNewTaskWindow.ShowDialog();
+
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+        var p = ProcessesDataGrid.SelectedItem as Process;
+
+        Process.GetProcessById(p.Id).Kill();
+        ProcessList = new(Process.GetProcesses().ToList());
+    }
+
+    private void AddBlackListClick(object sender, RoutedEventArgs e)
+    {
+        var p = ProcessesDataGrid.SelectedItem as Process;
+        BlackList.Add(p);
     }
 }
---
+++ Sys. Prog. Task 1 Task Manager/RunNewTaskWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+using System.Windows;
+
+
+namespace Sys._Prog._Task_1_Task_Manager;
+
+public partial class RunNewTaskWindow : Window
+{
+    public RunNewTaskWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void CancelButtonClick(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void OKButtonClick(object sender, RoutedEventArgs e)
+    {
+        Process.Start(TxtBox.Text);
+    }
+
+    private void BrowseButtonClick(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        openFileDialog.ShowDialog();
+        TxtBox.Text = openFileDialog.FileName;
+    }
+}
